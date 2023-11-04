ec8e4350ef09028c957ab552aed781faf12878bc
Matt Gacy <matt@mgacy.com>
2023-11-04 08:53:35 -0700

    Implement command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import TurndownService from 'turndown';
-import { Command } from '@commander-js/extra-typings';
-import { gfm } from "turndown-plugin-gfm";
+import TurndownService from "turndown";
+import { Command, Option } from "@commander-js/extra-typings";
+import { type } from "os";
+var turndownPluginGfm = require("turndown-plugin-gfm");
+
+const program = new Command()
+  .name("turndown-cli")
+  .description("Convert HTML to Markdown")
+  .version("0.1.0")
+  .argument("<html>", "HTML string or file path")
+  .action((html: string, options) => {
+    const markdown = markdownify(html);
+
+    console.log(markdown);
+  });
+
+function markdownify(html: string, options?: TurndownService.Options): string {
+  const turndownService = new TurndownService(options);
+
+  // GFM
+  var gfm = turndownPluginGfm.gfm;
+  turndownService.use(gfm);
+
+  return turndownService.turndown(html);
+}
+
+program.parse(process.argv);
